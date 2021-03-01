aa41ea5eed454f929c610b2e41f5e0c11dac5ca8
meddykip <mer671@nyu.edu>
2021-02-28 21:05:51 -0500

    added the block + code which kickstarts the whole machine!! >:)

---
+++ Assets/scripts/PUSHER.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PUSHER : MonoBehaviour
+{
+    // FOR PHYSICS CONTROL
+    Rigidbody2D myBody; //the body...
+    public float power; //to adjust the power >:)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+        //myBody will hold the Rigidbody2D component...
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+           //myBody.AddForce((Vector3.right + Vector3.up) * power);
+            
+            myBody.AddForce((new Vector3(1, 0, 0) + new Vector3(0, 1 , 0)) * power); 
+            // math...????   
+            // to go woosh with physics >:) ...   
+
+            Debug.Log("WOOSH");
+            // to prove it DID push and IS pushing..!! 
+        }
+    }
+
+
+}
--- Assets/scripts/ballBehavior.cs
+++ Assets/scripts/ballBehavior.cs
@@ -13,34 +13,21 @@ public class ballBehavior : MonoBehaviour
     public Color waterColor;
     public Color sunColor;
     public Color loveColor;
-
-    // FOR PHYSICS CONTROL
-    Rigidbody2D myBody; //the ball's body...
-    public float power; //to adjust the ball's power >:)
-
+    
     // Start is called before the first frame update
     void Start()
     {
         myRenderer = gameObject.GetComponent<SpriteRenderer>();
         //myRenderer will hold the sprite!
-
-        myBody = gameObject.GetComponent<Rigidbody2D>();
-        //myBody will hold the Rigidbody2D component...
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
-           //myBody.AddForce((Vector3.right + Vector3.up) * power);
-            
-            myBody.AddForce((new Vector3(1, 0, 0) + new Vector3(0, 1 , 0)) * power); 
-            // math...????   
-            // it makes the ball goes woosh with physics >:) ...    
-        }
+
     }
 
-    // GATE COLLISIONS !
+    // GATE COLLISIONS + COLOR CHANGERS!
     void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.name == "watergate"){ // if the ball collides with the WATER GATE...
             myRenderer.color = waterColor; // change it to water color!
